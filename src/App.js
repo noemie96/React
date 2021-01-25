1ba9c83d86cee2f57b588e16d7c909e74a150379
Nono <Admin@MBP-de-Test.mshome.net>
2021-01-25 10:53:42 +0100

    membre et age

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component, Fragment} from 'react';
+import './App.css'
+import Membre from './components/Membre'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const marauder = {
+  membre1:{
+    nom: 'Moony',
+    age: 34
+  },
+  membre2:{
+    nom: 'Wormtail',
+    age: 33
+  },
+  membre3:{
+    nom: 'Padfoot',
+    age: 34
+  },
+  membre4:{
+    nom: 'Prongs',
+    age: 24
+  },
 }
 
+class App extends Component {
+  state = {
+    marauder : marauder 
+  }
+
+  handleClick = () => {
+    const marauder= {...this.state.marauder}//...permet de copier un state car on ne peut pas le modifier directement
+    marauder.membre1.age += 1
+    this.setState({marauder:marauder})
+
+  }
+
+  render() { 
+    const {title} = this.props
+    return(
+      <Fragment>
+      <div className="App">
+        <h1>{this.props.title}</h1>
+        <h2>{title}</h2>
+        <p>I solemnly swear that i am up to no good! </p>
+      </div>
+      <Membre
+        age={this.state.marauder.membre1.age}
+        nom={this.state.marauder.membre1.nom}/>
+      <Membre 
+        age={this.state.marauder.membre2.age}
+        nom={this.state.marauder.membre2.nom}/>
+      <Membre
+        age={this.state.marauder.membre3.age}
+        nom={this.state.marauder.membre3.nom}/>
+      <Membre 
+        age={this.state.marauder.membre4.age}
+        nom={this.state.marauder.membre4.nom}/>
+        <Membre
+          age="13"
+          nom="Harry">
+          Je suis une merde
+        </Membre>
+        <button onClick={this.handleClick}>+1</button>
+        
+      </Fragment>
+   //   React.createElement('div', {className: 'App'}, React.createElement('h1',null,'React App'))
+    )
+}
+}
+ 
 export default App;
---
+++ src/components/Membre.js
@@ -0,0 +1,29 @@
+import React from 'react'; // quand on n'indique rien sur le path, on fait ref à node_modules
+
+//const Membre = (props) => {
+    
+ //   return ( 
+//        <>
+ //           <h2>Membre  : {props.nom.toUpperCase()}</h2>
+//        </>
+//     );
+//}
+const Membre = ({nom, children, age}) => {
+    
+    return ( 
+        <>
+            <h2>Membre  : {nom.toUpperCase()} - {age} ans</h2>
+            {children ? <p>{children}</p> : null}
+        </>
+     );
+}
+
+// const Membre = () => (
+//     <h2>Membre de la famille</h2>
+// )
+
+// const Membre = () => {
+//     return <h2>Membre de la famille</h2>
+// }
+ 
+export default Membre;
